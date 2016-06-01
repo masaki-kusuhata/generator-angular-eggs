14abbe72f953996269760572e4153d696061b6c5
FKN_kusuhata <kusuhata.masaki@jp.fujitsu.com>
2016-06-01 10:39:52 +0900

    不要な処理削除とコピーファイル名の修正

--- componentsTemplate/index.js
+++ componentsTemplate/index.js
@@ -1,28 +1,21 @@
 'use strict';
-
-var join = require('path').join;
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
-
-function dashCase(str) {
-    return str.replace(/[A-Z]/g, function (match) { return '-' + match.toLowerCase(); });
-}
 
 module.exports = yeoman.Base.extend({
   constructor: function () {
     yeoman.Base.apply(this, arguments);
     var text = arguments[0][0];
     this.text = text;
-    this.filename = text.toLowerCase();
     this.directoryname = text.toLowerCase();
   },
   js: function () {
-    this.bulkCopy('_components.js', 'templates/components/' + this.directoryname + '/' + this.filename + '.js');
+    this.bulkCopy('_components.js', 'templates/components/' + this.directoryname + '/_components.js');
   },
   html: function () {
-    this.bulkCopy('_components.html', 'templates/components/' + this.directoryname + '/' + this.filename + '.html');
+    this.bulkCopy('_components.html', 'templates/components/' + this.directoryname + '/_components.html');
   },
   test: function () {
-    this.bulkCopy('_components.spec.js', 'templates/components/' + this.directoryname + '/' + this.filename + '.spec.js');
+    this.bulkCopy('_components.spec.js', 'templates/components/' + this.directoryname + '/_components.spec.js');
   }
 });
