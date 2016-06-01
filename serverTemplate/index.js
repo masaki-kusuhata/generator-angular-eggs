753bbe9e2636ab54ba0d38db1bcc9f5d0e7fb246
FKN_kusuhata <kusuhata.masaki@jp.fujitsu.com>
2016-06-01 10:51:23 +0900

    server用template作成サブジェネレーター作成

---
+++ serverTemplate/index.js
@@ -0,0 +1,16 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+module.exports = yeoman.Base.extend({
+  constructor: function () {
+    yeoman.Base.apply(this, arguments);
+    var text = arguments[0][0];
+    this.text = text;
+    this.directoryname = text.toLowerCase();
+  },
+  js: function () {
+    this.bulkCopy('_api.js', 'templates/server/'+ this.directoryname +'/api/_api.js');
+    this.bulkCopy('_data.json', 'templates/server/'+ this.directoryname +'/data/_data.json');
+  }
+});
